a99ea33d57aaaf22200d2f7bdacb383369b68561
zsoltdzsugan <zsolt.dzsugan@gmail.com>
2025-12-22 21:52:41 +0100

    feat: soft account delete

--- lib/core/auth/provider.dart
+++ lib/core/auth/provider.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:messenger/core/controller/session.dart';
 
 class AuthProvider extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -14,10 +15,21 @@ class AuthProvider extends ChangeNotifier {
 
   AuthProvider() {
     _user = _auth.currentUser;
-    _sub = _auth.authStateChanges().listen((user) {
-      _user = user;
+    _sub = _auth.authStateChanges().listen((user) async {
+      if (user == null) {
+        _user = null;
+      } else {
+        try {
+          await user.reload();
+          _user = _auth.currentUser;
+        } catch (e) {
+          await _auth.signOut();
+          _user = null;
+        }
+      }
       notifyListeners();
     });
+    SessionController.instance.register(_sub!);
   }
 
   @override
