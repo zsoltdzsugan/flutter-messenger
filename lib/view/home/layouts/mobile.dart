398447d01375d3fb3316fadf1542e48e30562404
zsoltdzsugan <zsolt.dzsugan@gmail.com>
2025-12-22 22:14:00 +0100

    no meaningful change

--- lib/view/home/layouts/mobile.dart
+++ lib/view/home/layouts/mobile.dart
@@ -1,8 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:messenger/core/enums/sidebar_filter.dart';
 import 'package:messenger/core/extensions/design_extension.dart';
 
 class HomeMobileLayout extends StatelessWidget {
-  const HomeMobileLayout({super.key});
+  final TextEditingController searchController;
+  final SidebarFilter filter;
+  final bool isSidebarOpen;
+  final DocumentReference? activeConversation;
+  final String? otherUserId;
+  final Map<String, Map<String, dynamic>> usersCache;
+
+  final ValueChanged<SidebarFilter> onFilterChanged;
+  final VoidCallback onSidebarToggle;
+  final ValueChanged<DocumentReference> onConversationSelected;
+  final void Function(String uid) onUserNeeded;
+
+  const HomeMobileLayout({
+    super.key,
+    required this.filter,
+    required this.isSidebarOpen,
+    required this.searchController,
+    required this.usersCache,
+    this.activeConversation,
+    this.otherUserId,
+    required this.onFilterChanged,
+    required this.onConversationSelected,
+    required this.onSidebarToggle,
+    required this.onUserNeeded,
+  });
 
   @override
   Widget build(BuildContext context) {
